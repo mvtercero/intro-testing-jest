8f45c267b6ac95b9ac2f95a1b404926704d7d320
mvtercero <mvtercero85@gmail.com>
2018-05-06 21:21:19 +0200

    Created test suite Sum App with two new it

--- index.test.js
+++ index.test.js
@@ -1,5 +1,19 @@
 const sum = require('./index.js');
 
-test('adds 1 + 2 to equal 3', () => {
+
+describe ('Sum App',()=> {
+  it('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+  expect(sum(3, 4)).toBe(7);
+  });
+
+  it('adds positive and negative numbers',() => {
+  expect(sum(5,-3)).toBe(2);
+  expect(sum (2,-4)).toBe(-2);
+  });
+
+  it('adds decimal numbers',()=> {
+  expect(sum(1.2, 2.3)).toBe(3.5);
+  expect(sum(0.1,-0.2)).toBe(-0.1);
+  });
 });
